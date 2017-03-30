f4f4ba3e00076db701754fdc7d75502dd5486695
Terence <wangtaowxs@gmail.com>
2017-03-30 18:00:20 +0200

    Fix SignUp Bug

--- app/src/main/java/com/example/lb/lovebox/SignupActivity.java
+++ app/src/main/java/com/example/lb/lovebox/SignupActivity.java
@@ -95,7 +95,7 @@ public class SignupActivity extends AppCompatActivity {
 
     public void onSignupSuccess(String name, String email, String password) {
         _signupButton.setEnabled(true);
-//        AVUser user = new AVUser();// 新建 AVUser 对象实例
+        AVUser user = new AVUser();// 新建 AVUser 对象实例
         user.setUsername(name);// 设置用户名
         user.setPassword(password);// 设置密码
         user.setEmail(email);//设置邮箱
